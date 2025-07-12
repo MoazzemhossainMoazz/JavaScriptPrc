1cb51f4a2408861f324827527819f9932ca819fb
unknown <moazzemh222@gmail.com>
2025-07-12 16:33:25 +0600

    Personal Tracker Phase

---
+++ personalTrackerPhase3.js
@@ -0,0 +1,8 @@
+const userName = 'Moazzem hossain';
+const income = 3750.25;
+const expense =  2580.5;
+
+console.log('Personla Budget Tracker App');
+console.log("User : " + userName);
+console.log("Total Income: " + income);
+console.log("Total Expense: " + expense);
