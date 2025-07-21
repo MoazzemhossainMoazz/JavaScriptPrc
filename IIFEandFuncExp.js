d462b7909fcf66532d205d9b0a857e174c196fc3
unknown <moazzemh222@gmail.com>
2025-07-21 18:12:58 +0600

    IIFEandFuncExp

---
+++ IIFEandFuncExp.js
@@ -0,0 +1,33 @@
+// function display(){
+//     document.write("hello");
+//     console.log("hello world");
+// }
+// display();
+
+// --------------------------------------------
+
+// (function display(){
+//     console.log("hello");
+//     document.write("hello world");
+// }) ();
+
+// -----------------------------------------------
+
+// (function display(message){
+//     console.log(message);
+// }) ("hello");
+
+// -----------------------------------------------
+
+// //function expression
+// function displayMessage(){
+//     console.log("Hi this is me...");
+// }
+// displayMessage();
+
+
+// ----------------------------------------------
+var display2 = function displayMessage(msg){
+console.log(msg);
+};
+display2("this is messege");
