5a1f8121f5676361b28e0cd3a2ceca8c0a49bf88
unknown <moazzemh222@gmail.com>
2025-07-21 19:32:30 +0600

    Loop Array

---
+++ LoopArray.js
@@ -0,0 +1,12 @@
+var num = new Array();
+
+for(var i=0; i<5; i++){
+    num[i] = parseInt(prompt("Enter a number: "));
+}
+var sum = 0;
+
+for( var i = 0; i < 5; i++){
+    console.log(num[i]);
+    sum += num[i];
+}
+console.log(sum);
