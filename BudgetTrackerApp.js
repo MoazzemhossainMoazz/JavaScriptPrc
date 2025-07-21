b5785512a8e00a02427eceb5aca2f1f56606f91a
unknown <moazzemh222@gmail.com>
2025-07-21 15:45:46 +0600

    Traditional Function

--- BudgetTrackerApp.js
+++ BudgetTrackerApp.js
@@ -1,44 +1,61 @@
-var userName = "Moazzem Hossain";
-var income = 5000;
+
 var userName = prompt("Enter a user name: ");
 var income = prompt("Total income: ");
 var NumberofExpenses = parseInt(prompt("How many expenses do you have: "));
 
-for(i=1; i<=product; i++){
-    product = 
-    expense = expense +i;
-}
-let rent = 4200;
-let groceries = 600;
-let transport = 200;
-let entertainment = 300;
+if(isNaN(income) || isNaN(NumberofExpenses) || income <= 0 || NumberofExpenses < 0)
+    console.log("Invalid input, please enter valid numbers.");
+else{
+    let totalExpenses = 0;
+    for(let i=1; i<=NumberofExpenses; i++){
+        let expense = parseFloat(prompt(`Enter expense ${i}: `));
 
-var Expense = rent + groceries + transport + entertainment;
+        if(isNaN(expense) || expense < 0){
+            console.log(`Invalid input for expense ${i}`);
+            expense = 0;
+        }
+        totalExpenses += expense;
+    }
 
-document.write("Total Income: $" + income + "<br>");
-document.write("Total Expense: $" + Expense + "<br>");
+    
+console.log("Total Income: $" + income );
+console.log("Total Expense: $" + expense );
 
 var textdeduct = income * (10/100);
-document.write("Text Deducted (10%) : $" + textdeduct + "<br>");
+console.log("Text Deducted (10%) : $" + textdeduct + "<br>");
 
 var NetIncome = income - textdeduct;
-document.write("Net Income After Tex: $" + NetIncome + "<br>");
+console.log("Net Income After Tex: $" + NetIncome + "<br>");
 
-var RemBalance = (income - (Expense + textdeduct));
-document.write("Remaining Balance: $" + RemBalance + "<br>");
+var RemBalance = (income - (expense + textdeduct));
+console.log("Remaining Balance: $" + RemBalance + "<br>");
 
 var Savings = RemBalance * (20/100);
-document.write("Savings (20% of balance): $" + Savings + "<br>");
+console.log("Savings (20% of balance): $" + Savings + "<br>");
 
 if (Savings >= 1000)
-    document.write("Excellent" + "<br>");
+    console.log("Excellent" + "<br>");
 else if(Savings >500 && Savings < 999)
-    document.write("Good" + "<br>");
+    console.log("Good" + "<br>");
 else if(Savings >100 && Savings < 499)
-    document.write("Needs Improvement." + "<br>");
+    console.log("Needs Improvement." + "<br>");
 else if(Saving < 100)
-    document.write("Critical" + "<br>");
+    console.log("Critical" + "<br>");
 
 var overSpendingMessege = '';
-if(Expense > income)
-    document.write("Warning!! Over Expense");
+if(totalExpenses > income){
+    console.log("Warning!! Over Expense");
+}
+    
+
+console.log(`User: ${userName}`);
+console.log(`Total income: $${income}`);
+console.log(`Total expense: $${totalExpenses}`);
+console.log(`Text deduct: $${textdeduct}`);
+console.log(`Net income: $${NetIncome}`);
+console.log(`Balance: $${RemBalance}`);
+console.log(`Svaing: $${Savings}`);
+
+
+}
+
---
+++ traditionalFunction.js
@@ -0,0 +1,18 @@
+function square(num1, num2){
+    var result = num1 * num2;
+    document.write("Multiplication = " + result + "<br>");
+}
+
+function addition(num1, num2){
+    var result = num1 + num2;
+    document.write("Summation is = " + result + " <br> ");
+}
+
+function mult(num1, num2){
+    var result = num1 * num2;
+    document.write("Multiply is =" + result + "<br>");
+}
+
+square(10, 5);
+addition(10, 5);
+mult(10, 20);
