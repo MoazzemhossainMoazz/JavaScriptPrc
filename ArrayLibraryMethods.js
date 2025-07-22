a6079bc61884cfacd32483f9ce079d80a1395e63
unknown <moazzemh222@gmail.com>
2025-07-22 12:21:43 +0600

    Array Library Methods

---
+++ ArrayLibraryMethods.js
@@ -0,0 +1,44 @@
+var names = ["Rakib", "Sakib", "Tamim", "Afif", "Sajib"];
+console.log(names);
+
+// //shift opposite of pop
+// names.shift();
+// console.log(names);
+
+// names.shift();
+// console.log(names);
+
+// //unshift opposite of push
+// names.unshift("Hasan");
+// console.log(names);
+
+
+//adding elements using splice
+// names.splice(2,1, "Karim", "Rahim");
+// console.log(names);
+
+//removing elements using splice
+// names.splice(1,2);
+// console.log(names);
+
+//slice
+// var newArray = names.slice(1);
+// console.log(newArray);
+// console.log(names);
+
+//use Sorting function
+// var sortedNames = names.sort();
+// console.log(sortedNames);
+
+//use sorting use reverse
+// var sortedNames = names.sort();
+// console.log(sortedNames);
+// var revSortedNames = names.reverse();
+// console.log(revSortedNames);
+
+var numbers = [10, 50, 30, 40, 20, 70];
+console.log(numbers);
+numbers.sort();
+console.log(numbers);
+numbers.reverse();
+console.log(numbers);
